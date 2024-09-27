4e3d0f9f4b3b33b9a0f43f757c01b75644c78d0a
tiffany <tbcasper16@gmail.com>
2024-09-27 17:30:37 -0500

    task 4 - server actions complete!

--- components/AskQuestion.tsx
+++ components/AskQuestion.tsx
@@ -1,6 +1,9 @@
+import { addQuestion } from "@/lib/actions";
+
 export function AskQuestion({ topic }: { topic: string }) {
   return (
-    <form className="relative my-8">
+    <form className="relative my-8" action={addQuestion}>
+      <input type="hidden" name="topic_id" value={topic} className="hidden" />
       <input
         type="text"
         name="title"
@@ -13,3 +16,4 @@ export function AskQuestion({ topic }: { topic: string }) {
     </form>
   );
 }
+
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,1 +1,46 @@
 // Define your server actions here
+
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { insertQuestion, insertTopic, incrementVotes } from "./data";
+import { redirect } from "next/navigation";
+
+export async function addTopic(data: FormData) {
+    let topic;
+    try {
+        topic = await insertTopic({
+            title: data.get("title") as string,
+        });
+    } catch (error) {
+        console.error("Database Error:", error);
+        throw new Error("Failed to add topic.");
+    } finally {
+        revalidatePath("/ui/topics/[id]", "page");
+        topic && redirect(`/ui/topics/${topic.id}`);
+    }
+}
+
+export async function addQuestion(question: FormData) {
+    try {
+        insertQuestion({
+            title: question.get("title") as string,
+            topic_id: question.get("topic_id") as string,
+            votes: 0,
+        });
+        revalidatePath("/ui/topics/[id]", "page");
+    } catch (error) {
+        console.error("Database Error:", error);
+        throw new Error("Failed to add question.");
+    }
+}
+
+export async function addVote(data: FormData) {
+    try {
+        incrementVotes(data.get("id") as string);
+        revalidatePath("/ui/topics/[id]", "page");
+    } catch (error) {
+        console.error("Database Error:", error);
+        throw new Error("Failed to add vote.");
+    }
+}
